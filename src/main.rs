20e5b10a7d7b62d60f3cf39995ba62cb1b1d0e39
tachikawa <tachikawa@feedforce.jp>
2021-06-19 21:32:08 +0900

    feat: add example that return json response

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MyResponse {
+    str: String,
+    num: isize,
+    arr: Vec<isize>,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello, World!")
+}
+
+#[get("/json")]
+async fn json() -> impl Responder {
+    HttpResponse::Ok().json(MyResponse {
+        str: "テスト".to_string(),
+        num: 100,
+        arr: vec![1, 2, 3],
+    })
 }
 
 #[post("/echo")]
@@ -20,6 +37,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(json)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind("0.0.0.0:8080")?
